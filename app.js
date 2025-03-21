2ac4be6d7be2e01cbf567e90fc8832b5f797d6db
Andy Hall <andersonhall@gmail.com>
2025-03-21 17:53:21 -0500

    implement login/register and CRUD for users

--- app.js
+++ app.js
@@ -71,6 +71,82 @@ app.delete("/products/:productId", async (req, res) => {
   return res.status(204).send();
 });
 
+// register a user
+app.post("/register", async (req, res, next) => {
+  const { password, email, first_name, last_name } = req.body;
+  let user = await db.query(`SELECT * FROM users where email = $1`, [email]);
+  if (user.rows.length > 0) {
+    return res
+      .status(400)
+      .send({ message: "User already exists with that email address." });
+  }
+  user = await db.query(
+    `INSERT INTO users (password, email, first_name, last_name) VALUES ($1, $2, $3, $4) RETURNING *`,
+    [password, email, first_name, last_name]
+  );
+  return res.status(201).send(user.rows[0]);
+});
+
+// login user
+app.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await db.query(
+    `SELECT * from users WHERE email = $1 AND password = $2`,
+    [email, password]
+  );
+  if (user.rows.length === 0) {
+    return res.send({ message: "Email and password combination not found." });
+  }
+  res.send({ message: "User logged in.", user: user.rows[0] });
+});
+
+// get users
+app.get("/users", async (req, res) => {
+  const result = await db.query(`SELECT * FROM users ORDER BY user_id`);
+  res.send(result.rows);
+});
+
+// get user by id
+app.get("/users/:userId", async (req, res) => {
+  const { userId } = req.params;
+  const result = await db.query(`SELECT * FROM users WHERE user_id = $1`, [
+    userId,
+  ]);
+  if (result.rows.length === 0) {
+    return res.status(404).send({ message: "User not found" });
+  }
+  res.status(200).send(result.rows[0]);
+});
+
+// update user
+app.put("/users/:userId", async (req, res) => {
+  const { userId } = req.params;
+  const { email, password, first_name, last_name } = req.body;
+  const result = await db.query(
+    `UPDATE users SET email = $1, password = $2, first_name = $3, last_name = $4 WHERE user_id = $5 RETURNING *`,
+    [email, password, first_name, last_name, userId]
+  );
+  if (result.rows.length === 0) {
+    return res.status(404).send({ message: "User not found" });
+  }
+  return res
+    .status(200)
+    .send({ message: "User updated", user: result.rows[0] });
+});
+
+// delete user
+app.delete("/users/:userId", async (req, res) => {
+  const { userId } = req.params;
+  const result = await db.query(
+    `DELETE FROM users WHERE user_id = $1 RETURNING *`,
+    [userId]
+  );
+  if (result.rows.length === 0) {
+    return res.status(404).send({ message: "User not found" });
+  }
+  return res.status(204).send();
+});
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
